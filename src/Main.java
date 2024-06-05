58f9b0955ebb50404582e35915b1766c05cd607e
Ravindra <abtechreair1@gmail.com>
2024-06-05 09:19:55 +0530

    04-06-24 --  Create first introduction of java file

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        /* Variable
+           a-z ,A-Z , boy123 , boy_9,
+         */
+
+        int i, boy = 10 ;
+        System.out.println(boy);
+        i=i+10;
+
+        //Array
+
+        int students = [10,30,40,50]
+
+        char name = ["harish","ramesh","dinesh"];
+
+        System.out.println(students);
+
+       for (int i = 1; i <= 2000; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
